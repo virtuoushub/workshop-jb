beed9fb9b3ef445261d8449560de7f09a0ce9458
Peter Colapietro <petercolapietro@gmail.com>
2014-12-16 11:44:37 -0500

    Got i_introduction._5_Nullable_Types._05_Nullable_Types passing.

--- src/i_introduction/_5_Nullable_Types/NullableTypes.kt
+++ src/i_introduction/_5_Nullable_Types/NullableTypes.kt
@@ -64,7 +64,9 @@ fun todoTask5(client: Client?, message: String?, mailer: Mailer) = TODO(
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask5(client, message, mailer)
+    if(client != null && message != null && client.personalInfo != null && client.personalInfo.email != null) {
+        mailer.sendMessage(client.personalInfo.email, message)
+    }
 }
 
 class Client (val personalInfo: PersonalInfo?)
